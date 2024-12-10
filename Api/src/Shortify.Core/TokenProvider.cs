f888804e618241344968e019124b3dc6c09d7e3f
chathura liyanage <154213j@uom.lk>
2024-12-11 01:58:05 +0530

    unit tests added for generate short url feature

---
+++ Api/src/Shortify.Core/TokenProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace Shortify.Core;
+
+public class TokenProvider
+{
+	private int _start;
+	public void AssignRange(int start, int i1)
+	{
+		_start = start;
+	}
+
+	public int GetToken()
+	{
+		return _start;
+	}
+}

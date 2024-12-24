c48635649ac32e55f05a46c036cc132c1c4d996e
chathura liyanage <chathura070@gmail.com>
2024-12-24 23:15:20 +0530

    Token range service (#6)
    * token service ranges
    
    * token ranges changes

--- Api/src/Shortify.Api/Program.cs
+++ Api/src/Shortify.Api/Program.cs
@@ -34,6 +34,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapGet("/", () =>"API"); 
 app.MapPost("/api/urls", async (
 	AddUrlHandler handler,
 	AddUrlRequest request,

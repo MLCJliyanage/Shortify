54320bc457a09cd79e170b621e7df255b0613ef6
chathura liyanage <154213j@uom.lk>
2025-01-13 14:19:42 +0530

    web app changes

---
+++ WebApp/shortify-web-app/src/ListUrls/ListUrls.js
@@ -0,0 +1,19 @@
+import "./ListUrls.css"
+import React from "react";
+
+function ListUrls({ urls, continuationToken, onLoadMore }) {
+    return (
+        <div className="url-list">
+            {urls && urls.map((url) => (
+                <div key={url.shortUrl} className="url-item">
+                    {url.shortUrl} → {url.longUrl}
+                </div>
+            ))}
+            {continuationToken && (
+                <button onClick={onLoadMore}>Load more</button>
+            )}
+        </div>
+    );
+}
+
+export default ListUrls
--- WebApp/shortify-web-app/src/LogoutButton/LogoutButton.js
+++ WebApp/shortify-web-app/src/LogoutButton/LogoutButton.js
@@ -1,11 +1,12 @@
+import "./LogoutButton.css";
 import React from "react";
 
 function LogoutButton({ onLogout }) {
-  return (
-    <button onClick={onLogout} className='logout'>
-      Logout
-    </button>
-  );
+
+    return (
+        <button onClick={onLogout} className="logout">Logout</button>
+    );
+
 }
 
-export default LogoutButton;
+export default LogoutButton
